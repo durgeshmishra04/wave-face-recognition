6e4714f85da92aeb0845d34a2deb58e1e60cfe94
durgeshmishra04 <hellodurgeshmishra1998@gmail.com>
2026-08-21 10:50:59 +0530

    firebase

--- firebase_server.py
+++ firebase_server.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("C:/Users/AVANISH/Downloads/wave_fr/pythonai.json")
+cred = credentials.Certificate("./pythonai.json")
 firebase_admin.initialize_app(cred)
 
 message = messaging.Message(
